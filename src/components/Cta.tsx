c2cc751b0009b7679294f11bcd2d068bb9d93b20
Aziz-boy <shavgoniaziz@gmail.com>
2025-05-02 18:57:13 +0500

    fix: edited testimonial section

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -2,10 +2,7 @@ import { Button } from "./ui/button";
 
 export const Cta = () => {
   return (
-    <section
-      id="cta"
-      className="bg-muted/50 py-16 my-24 sm:my-32"
-    >
+    <section id="cta" className="bg-muted/50 py-16 my-24 sm:my-32">
       <div className="container lg:grid lg:grid-cols-2 place-items-center">
         <div className="lg:col-start-1">
           <h2 className="text-3xl md:text-4xl font-bold">
@@ -17,19 +14,24 @@ export const Cta = () => {
             Bitta Platformada
           </h2>
           <p className="text-muted-foreground text-xl mt-4 mb-8 lg:mb-0">
-            Boltaev School sizga kerakli barcha ta’lim yo‘nalishlarini bir joyda taqdim etadi:
-            Ingliz tili, IELTS, CEFR, huquq va tarix. O‘rganishni biz bilan boshlang!
+            Boltaev School sizga kerakli barcha ta’lim yo‘nalishlarini bir joyda
+            taqdim etadi: Ingliz tili, IELTS, CEFR, huquq, tarix matematika, ona
+            tili. O‘rganishni biz bilan boshlang!
           </p>
         </div>
 
         <div className="space-y-4 lg:col-start-2">
-          <Button className="w-full md:mr-4 md:w-auto">Demo darsni so‘rash</Button>
-          <Button
-            variant="outline"
-            className="w-full md:w-auto"
-          >
-            Barcha xususiyatlar
-          </Button>
+          <a href="https://t.me/+998883070555" target="_blank" rel="noopener noreferrer">
+            <Button className="w-full md:mr-4 md:w-auto">
+              Demo darsga qatnashish
+            </Button>
+          </a>
+
+          <a href="https://t.me/boltaev_huquq" target="_blank" rel="noopener noreferrer">
+            <Button variant="outline" className="w-full md:w-auto">
+              Bizni kuzatish
+            </Button>
+          </a>
         </div>
       </div>
     </section>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -16,54 +16,57 @@ interface TestimonialProps {
 
 const testimonials: TestimonialProps[] = [
   {
-    image: "https://github.com/shadcn.png",
-    name: "John Doe React",
-    userName: "@john_Doe",
-    comment: "Bu landin sahifa juda zo'r!",
+    image:
+      "https://i.ibb.co/Jw8XR8N1/telegram-cloud-photo-size-2-5235805484945107935-x.jpg",
+    name: "Afzalshoh Hojiyev",
+    userName: "Toshkent Davlat Yuridik Universiteti",
+    comment:
+      "Boltaev School o'quv markazi yuqori malakali o'qituvchilar va innovatsion metodlar bilan ta'lim berib, talabalarning huquqshunoslik va tarix sohalarida yuqori natijalarga erishishlariga yordam beradi.",
   },
   {
-    image: "https://github.com/shadcn.png",
-    name: "John Doe React",
-    userName: "@john_Doe1",
+    image:
+      "https://i.ibb.co/3ysyZpGD/telegram-cloud-photo-size-2-5228932888731445180-c.jpg",
+    name: "Azizbek Hakimov",
+    userName: "@AzizboyFikrlog",
     comment:
-      "Lorem ipsum dolor sit amet, empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+      "Boltaev School innovatsion yondashuv va malakali o'qituvchilar bilan ta'lim sohasida yangi standartlar o'rnatmoqda.",
   },
   {
-    image: "https://github.com/shadcn.png",
-    name: "John Doe React",
-    userName: "@john_Doe2",
+    image:
+      "https://i.ibb.co/S7QMC4wh/telegram-cloud-photo-size-2-5235805484945107977-y.jpg",
+    name: "Baxshilloyev Inoyat",
+    userName: "Buxoro Davlat Universiteti",
     comment:
-      "Lorem ipsum dolor sit amet, exercitation. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+      "Boltaev Schoolda o‘qish juda qulay, o‘qituvchilar ham juda malakali. Darslar juda aniq va tushunarli bo‘ladi, har doim yordamlashishadi!",
   },
   {
-    image: "https://github.com/shadcn.png",
-    name: "John Doe React",
-    userName: "@john_Doe3",
+    image:
+      "https://i.ibb.co/d0ykpkbC/telegram-peer-photo-size-2-5451812288047739985-1-0-0.jpg",
+    name: "Asalbek Sadiyev",
+    userName: "INHA University Tashkent",
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+      "Boltaev Schoolning test tuzib beradigan botidan juda mamnunman. Tez va oson test yaratish imkoniyati beradi, darslarga tayyorlanish juda qulay!",
   },
   {
-    image: "https://github.com/shadcn.png",
-    name: "John Doe React",
-    userName: "@john_Doe4",
+    image:
+      "https://i.ibb.co/b50cMsFq/telegram-cloud-photo-size-2-5235805484945108045-x.jpg",
+    name: "Jake Smith",
+    userName: "INHA University South Korea",
     comment:
-      "Lorem ipsum dolor sit amet, tempor incididunt aliqua. Ut enim ad minim veniam, quis nostrud.",
+      "The guy who made this website is truly a talented guy. He's got real skills and did an amazing job. We were actually classmates back at Inha University, and it’s great to see how far he’s come",
   },
   {
-    image: "https://github.com/shadcn.png",
-    name: "John Doe React",
-    userName: "@john_Doe5",
+    image: "https://i.ibb.co/5WmyYgM3/telegram-cloud-photo-size-2-5226488717332573174-c.jpg",
+    name: "Javohir Oromov",
+    userName: "TATU",
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Тестовый генератор от Boltaev School — очень полезный инструмент. Идеальное средство для быстрого создания тестов и подготовки к занятиям!",
   },
 ];
 
 export const Testimonials = () => {
   return (
-    <section
-      id="testimonials"
-      className="container py-24 sm:py-32"
-    >
+    <section id="testimonials" className="container py-24 sm:py-32">
       <h2 className="text-3xl md:text-4xl font-bold">
         Nima Uchun
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
@@ -74,7 +77,8 @@ export const Testimonials = () => {
       </h2>
 
       <p className="text-xl text-muted-foreground pt-4 pb-8">
-        Boltaev Kurs Centeri talabalari va mijozlarining fikrlari asosida yaratilgan va ularning muvaffaqiyatiga hissa qo'shadi.
+        Boltaev Kurs Centeri talabalari va mijozlarining fikrlari asosida
+        yaratilgan va ularning muvaffaqiyatiga hissa qo'shadi.
       </p>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-4 sm:block columns-2  lg:columns-3 lg:gap-6 mx-auto space-y-4 lg:space-y-6">
@@ -86,11 +90,8 @@ export const Testimonials = () => {
             >
               <CardHeader className="flex flex-row items-center gap-4 pb-2">
                 <Avatar>
-                  <AvatarImage
-                    alt=""
-                    src={image}
-                  />
-                  <AvatarFallback>OM</AvatarFallback>
+                  <AvatarImage alt="" src={image} />
+                  <AvatarFallback>JO</AvatarFallback>
                 </Avatar>
 
                 <div className="flex flex-col">
